1d5034d16aea38a3db0fc31e5eeab69f1a7ebfcd
bhanu-512 <bhanuteja.5122@gmail.com>
2026-01-23 17:13:39 +0530

    jwt and bycrypt

--- src/app.js
+++ src/app.js
@@ -9,8 +9,8 @@ app.use(cors({
 app.use(express.json({
     limit:"16kb"
 }))
+app.get
 app.use(express.static("public"))
 app.use(cookieParser())
 app.use(express.urlencoded({extended:true,limit:"16kb"}))
-
 export {app}
--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -7,13 +7,17 @@ const asyncHandler=(RequestHandler)=>{
 
 export {asyncHandler}
 /*asynchandler using try catch block
+"It’s for wrapping async functions in try–catch by calling
+ through a higher-order function asyncHandler, so it 
+ returns the function async (req, res, next) that runs 
+ the code in a try–catch block.”
 const asyncHanler=(fn)=>async(req,res,next)=>{
 try {
     await fn(req,res,next)
 } catch (error) {
     res.status(error.code||500).json({
         success:false,
-        message:err.message
+        message:error.message
     })
 }
 }
